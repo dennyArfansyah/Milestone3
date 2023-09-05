8c61e4d776f70f64fe64d1e6cdb7efa2e5fd4261
dennyArfansyah <dennyarfansyah@gmail.com>
2023-09-05 11:25:30 +0700

    Initial Commit

---
+++ Milestone3/Milestone3App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone3App.swift
+//  Milestone3
+//
+//  Created by Denny Arfansyah on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Milestone3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
